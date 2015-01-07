7b03e05fa10b3d69c55705196fd6f9cf7a976d65
rajanpatil <rajan.rb@gmail.com>
2015-01-07 17:15:23 +0530

    Moved Hello.scala to controllers package.

--- app/controllers/Hello.scala
+++ app/controllers/Hello.scala
@@ -1,11 +1,10 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 object Hello extends Controller {
 
-def sayHello = Action { 
-Ok("Hello ScalaPlay!")
-}
+  def sayHello = Action {
+    Ok("Hello ScalaPlay!")
+  }
 }
